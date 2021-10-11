26b83237e9645320a9050aa1800a62a3ede18167
mostafa ayman <mostafaaymanpro@gmail.com>
2021-10-12 00:02:28 +0200

    Initial Commit

---
+++ MyLoaderView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLoaderView
+//
+//  Created by Mostafa Ayman on 12/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
